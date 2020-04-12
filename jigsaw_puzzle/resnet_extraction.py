352cc480014b477412c626e0fe838e9235c3d932
wfcb <williamcorrea85@gmail.com>
2020-04-12 12:24:19 -0400

    heriting from resnet to decrease code duplication

--- jigsaw_puzzle/resnet_extraction.py
+++ jigsaw_puzzle/resnet_extraction.py
@@ -3,7 +3,7 @@ import torch
 from torchvision.models.utils import load_state_dict_from_url
 
 from torchvision.models.resnet import conv1x1,  BasicBlock,\
-    Bottleneck, model_urls
+    Bottleneck, model_urls, ResNet
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
@@ -11,87 +11,7 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'wide_resnet50_2', 'wide_resnet101_2']
 
 
-class ResNetFeatureExtraction(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None):
-        super(ResNetFeatureExtraction, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-
-        self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-        self.groups = groups
-        self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-        # self.inplanesFusedExtraction = 512
-        # self.bnExtraction = norm_layer(self.inplanesFusedExtraction)
-        # self.lastLinear = nn.Linear(512, 8)
-
-
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
-
-        return nn.Sequential(*layers)
+class ResNetFeatureExtraction(ResNet):
 
     def _forward_impl(self, x):
         # See note [TorchScript super()]
@@ -106,8 +26,6 @@ class ResNetFeatureExtraction(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-        # x = torch.flatten(x, 1)
-        # x = self.fc(x)
 
         return x
 
@@ -115,24 +33,12 @@ class ResNetFeatureExtraction(nn.Module):
         return self._forward_impl(x)
 
     def fused_output(self,firstEmb, secondEmb):
-        # planes = 512
-
-        # bn = nn.BatchNorm2d(self.inplanes)
-
-        rel = nn.ReLU(inplace=True)
 
         fused_emb = firstEmb + secondEmb
 
-        # print("shape :", fused_emb.shape)
-
-        # fused_emb = self.bnExtraction(fused_emb)
-        fused_emb = rel(fused_emb)
-
-        # print("fused emb shape : ", fused_emb.shape)
+        fused_emb = self.relu(fused_emb)
 
         fused_emb = torch.flatten(fused_emb, 1)
-
-        # print("shape after flattering : ", fused_emb.shape)
 
         fused_emb = self.fc(fused_emb)
 
